792a4bd41c60ca5ba839b5e08131bbd53176107b
Thomas Fisher <tfisher013@gmail.com>
2024-02-08 08:37:01 -0700

    Specified categorical features in constant

--- utils/consts.py
+++ utils/consts.py
@@ -1,4 +1,12 @@
-categorical_columns =[ ""]
+categorical_features=["ProductCD",
+                      "card1", 
+                      "card2", 
+                      "card3", 
+                      "card4", 
+                      "card5", 
+                      "card6",
+                      "addr1", 
+                      "addr2"]
 confidence_interval = 0.05
 no_of_trees_in_forest = 0
 hyper_parameters = []
