3938154d8e4f6823ca97215f03ca7d1d70f0600c
Thomas Fisher <tfisher013@gmail.com>
2024-02-23 14:42:11 -0700

    Created function for reading Forest model from file

--- utils/model_file_utils.py
+++ utils/model_file_utils.py
@@ -1,5 +1,23 @@
 import pickle
 from tree.Tree import Tree
+from tree.Forest import Forest
+
+def read_tree_model(filepath: str) -> Forest:
+    """ Reads a Forest object from file and returns it.
+
+        Parameters:
+            filepath: the path to the file containing the forest model
+
+        Returns
+            the Forest object represented by the provided file
+    """
+
+    try:
+        pickled_model = open(filepath, "rb")
+        return pickle.load(pickled_model)
+    except Exception:
+        print('Error reading model from file. Exiting.')
+        exit(1)
 
 def read_tree_model(filepath: str) -> Tree:
     """ Reads a Tree object from file and returns it.
@@ -17,6 +35,4 @@ def read_tree_model(filepath: str) -> Tree:
     except Exception:
         print('Error reading model from file. Exiting.')
         exit(1)
-        
-
 
