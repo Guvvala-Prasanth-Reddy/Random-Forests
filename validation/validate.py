Corrected calculation of balanced error

validation/validate.py
@@ -5,24 +5,28 @@ from classification.classify import tree_classify, forest_classify
 from tree.Forest import Forest
 
 
-def get_balanced_error_efficient(true_targets: pd.DataFrame, pred_targets: pd.DataFrame) -> float:
-    print(true_targets)
-    positive_indices = true_targets.loc[true_targets[target_column] == 1].index
-    negative_indices = true_targets.loc[true_targets[target_column] == 0].index
+def get_balanced_error_efficient(true_targets_col: pd.DataFrame, pred_targets_col: pd.DataFrame) -> float:
+    """ Calculates balanced error in an efficient manner.
+    """
 
-    positive_true_targets = true_targets.iloc[positive_indices]
-    negative_true_targets = true_targets.iloc[negative_indices]
+    true_targets = true_targets_col.to_numpy()
+    pred_targets = pred_targets_col.to_numpy()
+    false_negative_rate, false_positive_rate = 0, 0
 
-    positive_predicted_targets = pred_targets.iloc[positive_indices]
-    negative_predicted_targets = pred_targets.iloc[negative_indices]
-
-    # get the number of matches between the positive and negative predictions
-    num_positive_matches = (positive_true_targets == positive_predicted_targets).sum()
-    num_negative_matches = (negative_true_targets == negative_predicted_targets).sum()
-
-    false_positive_rate = abs((len(true_targets) - num_negative_matches)) / len(true_targets)
-    false_negative_rate = abs((len(true_targets) - num_positive_matches)) / len(true_targets)
-
+    # case where all true targets are 0 
+    if np.sum(true_targets) == 0:
+        false_positive_rate = np.sum(pred_targets) / len(true_targets)
+    # case where all true targets are 1
+    elif np.sum(true_targets) == len(true_targets):
+        false_negative_rate = np.sum(np.logical_not(pred_targets)) / len(true_targets)
+    # case with both 0s and 1s in true targets
+    else:
+        false_negative_rate = 1 - (np.sum(
+            np.logical_and(true_targets, pred_targets)) / np.sum(true_targets))
+        false_positive_rate = 1 - (np.sum(np.logical_and(np.logical_not(true_targets), 
+                                                         np.logical_not(pred_targets))) 
+                                / (len(true_targets) - np.sum(true_targets)))
+		
     return 0.5 * (false_negative_rate + false_positive_rate)
 
 