a6f7caf051c6d1c4d6a80303417abf107e53b414
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2024-03-07 14:21:16 -0700

    Implemented util function to get tree depth

--- utils/model_file_utils.py
+++ utils/model_file_utils.py
@@ -1,6 +1,26 @@
 import pickle
 from tree.Tree import Tree
 from tree.Forest import Forest
+from tree.Leaf import Leaf
+import numpy as np
+
+def get_tree_depth(tree: Tree) -> int:
+    """ Returns the max depth of the provided tree model
+
+        Parameters:
+            tree: A Tree object
+
+        Returns:
+            the max depth of the provided tree object
+    """
+
+    if type(tree) is Leaf:
+        return 0
+    else:
+        tree_path_depths = np.zeros(len(tree.branches), dtype=np.uint16)
+        for idx, branch in enumerate(tree.branches):
+            tree_path_depths[idx] = 1 + get_tree_depth(branch.tree)
+        return np.max(tree_path_depths)
 
 def read_forest_model(filepath: str) -> Forest:
     """ Reads a Forest object from file and returns it.
