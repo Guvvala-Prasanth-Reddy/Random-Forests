39dd605e2164118c1ff4a52a87d697b4d5a2a791
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2024-03-06 23:19:39 -0700

    Added handling of missing values to CSV file generation

--- classification/classify.py
+++ classification/classify.py
@@ -5,6 +5,7 @@ import numpy as np
 import statistics as st
 import pandas as pd
 from utils.model_file_utils import *
+from training.build_decision_tree import handle_missing_values
 
 def tree_classify(df: pd.DataFrame, tree: Tree):
     """ Returns the decision tree classification of the given instance
@@ -51,7 +52,18 @@ def generate_predictions_file(model_file: str, output_file='output.csv'):
     """ Generates a CSV file of class predictions using a Forest model
     """
 
+    # read forest model from file
     forest_model = read_forest_model(model_file)
+
+    # read testing data and hanlde missing values
     testing_df = pd.read_csv('data/test.csv')
+    testing_df = handle_missing_values(testing_df)
+
+    # generate predictions, concat with transaction ids, and output to csv
     predictions = testing_df.apply(forest_classify, args=(forest_model), axis=1)
+    predictions_with_ids = pd.concat([testing_df.get('TransactionID'), predictions], axis=1).reset_index(drop=True)
     predictions.to_csv(output_file, index=False)
+
+# use this main function to generate CSV file by calling this file, just replace forest_model_name
+#if __name__ == '__main__':
+#    generate_predictions_file('models/forest_model_name', output_file='output.csv')
